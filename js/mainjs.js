8e8ae5269bfd00193bff0fd7d25cc84fbe86f996
laker <laker@ubuntu.ubuntu-domain>
2013-05-05 18:48:10 +0800

    test

---
+++ js/mainjs.js
@@ -0,0 +1,20 @@
+/*window.onload = function() {
+	setActBlockDd();
+}
+
+function setActBlockDd() {
+    var blockDd = Sizzle(".activitie_list_box_dl dd");
+    console.log(blockDd);
+    // blockDd.style.border = "1px solid #000";
+}*/
+
+/*
+$(document).ready(function() {
+	var a = $(".activitie_list_box").css("width");
+	var b = $(".activitie_list_box dt").css("width"); 
+    $(".activitie_list_box_dl dd").css("width", parseInt(a - b) - 20);
+    console.log(a);
+    console.log(b);
+    var c = $(".activitie_list_box_dl dd").css("width");
+    console.log(c);
+});*/
